603cdf2067b9621469a4f4c7ad1c5848e6886fd7
tmlganesh <tmlganesh24@gmail.com>
2025-08-22 09:55:12 +0530

    Clean up setup files and batch scripts

--- start-app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Prodiny Full Application (Backend + Frontend)...
-cd /d "%~dp0"
-npm run dev
--- start-frontend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Prodiny Frontend...
-cd /d "%~dp0\client"
-npm start
